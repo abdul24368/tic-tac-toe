9330aac73936c7a77fa0a1c4e58b8a5223f9b6fe
abdul24368 <abdulrehman24368@gmail.com>
2022-07-06 16:49:55 +0530

    unity project files

---
+++ tic tac toe/Assets/Script/GameScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    int spriteindex = -1;
+    public int PlayerTurn()
+    {
+        spriteindex++;
+        return spriteindex % 2;
+    }
+}
---
+++ tic tac toe/Assets/Script/TurnScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnScript : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    public Sprite[] images;
+    GameObject gameBoard;
+    bool unplayed = true;
+    private void Start()
+    {
+        spriteRenderer.sprite = null;
+    }
+    private void OnMouseDown()
+    {
+        if (unplayed) {
+            int index = gameBoard.GetComponent<GameScript>().PlayerTurn();
+            spriteRenderer.sprite = images[index];
+            unplayed = false;
+        }
+        
+    }
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        gameBoard = GameObject.Find("game board");
+    }
+}
